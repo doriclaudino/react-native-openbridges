87b3be98c9c7896a873720433b57448ee71492ce
dori claudino <dori.claudino@gmail.com>
2019-02-14 14:36:58 -0500

    phoneLogin

--- app/screens/LoginScreen.js
+++ app/screens/LoginScreen.js
@@ -1,16 +1,21 @@
-import React, { Component } from 'react'
-import {
-    StyleSheet,
-    View,
-    Text,
-    Button
-} from 'react-native'
-import { Appbar } from 'react-native-paper';
+import React, { Component } from 'react';
+import { View, Text, TextInput, Image } from 'react-native';
+import { Appbar, Button } from 'react-native-paper';
 import firebase from 'react-native-firebase';
+
+const successImageUri = 'https://cdn.pixabay.com/photo/2015/06/09/16/12/icon-803718_1280.png';
 
 export default class LoginScreen extends Component {
     constructor(props) {
         super(props);
+        this.unsubscribe = null;
+        this.state = {
+            user: null,
+            message: '',
+            codeInput: '',
+            phoneNumber: '+1 702 366 ',
+            confirmResult: null,
+        };
     }
 
     static navigationOptions = ({ navigation }) => {
@@ -24,29 +29,126 @@ export default class LoginScreen extends Component {
         })
     };
 
-    tryLogIn = () => {
-        console.log('trying login')
-        firebase.auth()
-            .signInAnonymously()
-            .then(credential => {
-                if (credential) {
-                    console.log('default app user ->', credential.user.toJSON());
-                }
-            });
+    componentDidMount() {
+        this.unsubscribe = firebase.auth().onAuthStateChanged((user) => {
+            if (user) {
+                this.setState({ user: user.toJSON() });
+            } else {
+                // User has been signed out, reset the state
+                this.setState({
+                    user: null,
+                    message: '',
+                    codeInput: '',
+                    phoneNumber: '+1 702 366 ',
+                    confirmResult: null,
+                });
+            }
+        });
+    }
+
+    componentWillUnmount() {
+        if (this.unsubscribe) this.unsubscribe();
+    }
+
+    signIn = () => {
+        const { phoneNumber } = this.state;
+        this.setState({ message: 'Sending code ...' });
+
+        firebase.auth().signInWithPhoneNumber(phoneNumber)
+            .then(confirmResult => this.setState({ confirmResult, message: 'Code has been sent!' }))
+            .catch(error => this.setState({ message: `Sign In With Phone Number Error: ${error.message}` }));
+    };
+
+    confirmCode = () => {
+        const { codeInput, confirmResult } = this.state;
+
+        if (confirmResult && codeInput.length) {
+            confirmResult.confirm(codeInput)
+                .then((user) => {
+                    this.setState({ message: 'Code Confirmed!' });
+                })
+                .catch(error => this.setState({ message: `Code Confirm Error: ${error.message}` }));
+        }
+    };
+
+    signOut = () => {
+        firebase.auth().signOut();
+    }
+
+    renderPhoneNumberInput() {
+        const { phoneNumber } = this.state;
+
+        return (
+            <View style={{ padding: 25 }}>
+                <Text>Enter phone number:</Text>
+                <TextInput
+                    autoFocus
+                    style={{ height: 40, marginTop: 15, marginBottom: 15 }}
+                    onChangeText={value => this.setState({ phoneNumber: value })}
+                    placeholder={'Phone number ... '}
+                    value={phoneNumber}
+                />
+                <Button onPress={this.signIn}>Sign In</Button>
+            </View>
+        );
+    }
+
+    renderMessage() {
+        const { message } = this.state;
+
+        if (!message.length) return null;
+
+        return (
+            <Text style={{ padding: 5, backgroundColor: '#000', color: '#fff' }}>{message}</Text>
+        );
+    }
+
+    renderVerificationCodeInput() {
+        const { codeInput } = this.state;
+
+        return (
+            <View style={{ marginTop: 25, padding: 25 }}>
+                <Text>Enter verification code below:</Text>
+                <TextInput
+                    autoFocus
+                    style={{ height: 40, marginTop: 15, marginBottom: 15 }}
+                    onChangeText={value => this.setState({ codeInput: value })}
+                    placeholder={'Code ... '}
+                    value={codeInput}
+                />
+                <Button onPress={this.confirmCode}>Confirm Code</Button>
+            </View>
+        );
     }
 
     render() {
+        const { user, confirmResult } = this.state;
         return (
-            <View style={styles.container}>
-                <Text>Login</Text>
-                <Button title="LOG-IN" onPress={this.tryLogIn}></Button>
+            <View style={{ flex: 1 }}>
+
+                {!user && !confirmResult && this.renderPhoneNumberInput()}
+
+                {this.renderMessage()}
+
+                {!user && confirmResult && this.renderVerificationCodeInput()}
+
+                {user && (
+                    <View
+                        style={{
+                            padding: 15,
+                            justifyContent: 'center',
+                            alignItems: 'center',
+                            backgroundColor: '#77dd77',
+                            flex: 1,
+                        }}
+                    >
+                        <Image source={{ uri: successImageUri }} style={{ width: 100, height: 100, marginBottom: 25 }} />
+                        <Text style={{ fontSize: 25 }}>Signed In!</Text>
+                        <Text>{JSON.stringify(user)}</Text>
+                        <Button onPress={this.signOut}>Sign Out</Button>
+                    </View>
+                )}
             </View>
-        )
+        );
     }
 }
-
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-    },
-})
