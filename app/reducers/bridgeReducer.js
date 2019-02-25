d243078e04dd5811dee1e329c5b994c8a5e49296
doriclaudino <dori.claudino@gmail.com>
2019-02-25 14:32:45 -0500

    fixed undefined events elements on spreding on reducing

--- app/reducers/bridgeReducer.js
+++ app/reducers/bridgeReducer.js
@@ -1,15 +1,21 @@
+import ActionTypes from '../actions'
+
 export const bridgeReducer = (state = [], action) => {
-    console.log(action)
+    console.log('bridgeReducer', action)
     switch (action.type) {
-        case 'ADD_BRIDGES':
+        case ActionTypes.FETCH_BRIDGES_SUCCESS:
             return action.payload;
-        case 'CLEAR_BRIDGES':
+        case ActionTypes.FETCH_BRIDGES:
+            return state;
+        case ActionTypes.CLEAR_BRIDGES:
             return [];
-        case 'ADD_BRIDGE_EVENT':
-            return [...removeItem(state, action.payload.bridge), { ...action.payload.bridge, events: new Array().concat(action.payload.bridge.events, action.payload.event) }]
-        case 'DEL_BRIDGE_EVENT':
+        case ActionTypes.ADD_BRIDGE_EVENT:
+            events = action.payload.bridge.events ? [...action.payload.bridge.events] : []
+            events.push(action.payload.event)
+            return [...removeItem(state, action.payload.bridge), { ...action.payload.bridge, events }]
+        case ActionTypes.DEL_BRIDGE_EVENT:
             return [...removeItem(state, action.payload.bridge), { ...action.payload.bridge, events: removeItem(action.payload.bridge.events, action.payload.event) }]
-        case 'ADD_BRIDGE_STATUS_THRESHOLD':
+        case ActionTypes.ADD_BRIDGE_STATUS_THRESHOLD:
             return [...removeItem(state, action.payload.bridge), { ...action.payload.bridge, statusThreshold: action.payload.statusThreshold }]
         default:
             return state;
