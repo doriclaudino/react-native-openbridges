edff905041cd35c30ffa50492c7ecc59ae010285
dori claudino <dori.claudino@gmail.com>
2019-03-18 14:08:05 -0400

    TSLint

--- app/components/DialogRadioButton.tsx
+++ app/components/DialogRadioButton.tsx
@@ -1,89 +1,93 @@
-import * as React from 'react';
-import { ScrollView, View, StyleSheet } from 'react-native';
+import * as React from 'react'
+import { ScrollView, View, StyleSheet } from 'react-native'
 import {
-    Subheading,
-    Button,
-    Portal,
-    Dialog,
-    RadioButton,
-    TouchableRipple,
-} from 'react-native-paper';
-import { TBridgeStatus } from 'store/bridge';
+  Subheading,
+  Button,
+  Portal,
+  Dialog,
+  RadioButton,
+  TouchableRipple,
+} from 'react-native-paper'
+import { TBridgeStatus } from 'store/bridge'
 
 interface Props {
-    checked: TBridgeStatus
-    visible: boolean
-    onClose: () => void
-    onConfirm: (checked: TBridgeStatus) => void
+  checked: TBridgeStatus
+  visible: boolean
+  onClose: () => void
+  onConfirm: (checked: TBridgeStatus) => void
 }
 
 interface State {
-    checked: TBridgeStatus
+  checked: TBridgeStatus
 }
 
 export default class extends React.Component<Props, State> {
-    state: State = {
-        checked: this.props.checked
-    };
+  state: State = {
+    checked: this.props.checked,
+  }
 
-    render() {
-        const { checked } = this.state;
-        const { visible, onClose, onConfirm } = this.props;
-        return (
-            <Portal>
-                <Dialog onDismiss={onClose} visible={visible}>
-                    <Dialog.Title>Choose an option</Dialog.Title>
-                    <Dialog.ScrollArea style={{ maxHeight: 170, paddingHorizontal: 0 }}>
-                        <ScrollView>
-                            <View>
-                                <TouchableRipple
-                                    onPress={() => this.setState({ checked: 'Open' })}
-                                >
-                                    <View style={styles.row}>
-                                        <View pointerEvents="none">
-                                            <RadioButton
-                                                value={'Open'}
-                                                status={checked === 'Open' ? 'checked' : 'unchecked'}
-                                            />
-                                        </View>
-                                        <Subheading style={styles.text}>Open</Subheading>
-                                    </View>
-                                </TouchableRipple>
-                                <TouchableRipple
-                                    onPress={() => this.setState({ checked: 'Close' })}
-                                >
-                                    <View style={styles.row}>
-                                        <View pointerEvents="none">
-                                            <RadioButton
-                                                value={'Close'}
-                                                status={checked === 'Close' ? 'checked' : 'unchecked'}
-                                            />
-                                        </View>
-                                        <Subheading style={styles.text}>Close</Subheading>
-                                    </View>
-                                </TouchableRipple>
-                            </View>
-                        </ScrollView>
-                    </Dialog.ScrollArea>
-                    <Dialog.Actions>
-                        <Button onPress={() => {
-                            onConfirm(checked);
-                        }}>Ok</Button>
-                    </Dialog.Actions>
-                </Dialog>
-            </Portal>
-        );
-    }
+  render() {
+    const { checked } = this.state
+    const { visible, onClose, onConfirm } = this.props
+    return (
+      <Portal>
+        <Dialog onDismiss={onClose} visible={visible}>
+          <Dialog.Title>Choose an option</Dialog.Title>
+          <Dialog.ScrollArea style={{ maxHeight: 170, paddingHorizontal: 0 }}>
+            <ScrollView>
+              <View>
+                <TouchableRipple
+                  onPress={() => this.setState({ checked: 'Open' })}
+                >
+                  <View style={styles.row}>
+                    <View pointerEvents="none">
+                      <RadioButton
+                        value={'Open'}
+                        status={checked === 'Open' ? 'checked' : 'unchecked'}
+                      />
+                    </View>
+                    <Subheading style={styles.text}>Open</Subheading>
+                  </View>
+                </TouchableRipple>
+                <TouchableRipple
+                  onPress={() => this.setState({ checked: 'Close' })}
+                >
+                  <View style={styles.row}>
+                    <View pointerEvents="none">
+                      <RadioButton
+                        value={'Close'}
+                        status={checked === 'Close' ? 'checked' : 'unchecked'}
+                      />
+                    </View>
+                    <Subheading style={styles.text}>Close</Subheading>
+                  </View>
+                </TouchableRipple>
+              </View>
+            </ScrollView>
+          </Dialog.ScrollArea>
+          <Dialog.Actions>
+            <Button
+              onPress={() => {
+                onConfirm(checked)
+              }}
+            >
+              Ok
+            </Button>
+          </Dialog.Actions>
+        </Dialog>
+      </Portal>
+    )
+  }
 }
 
 const styles = StyleSheet.create({
-    row: {
-        flexDirection: 'row',
-        alignItems: 'center',
-        paddingHorizontal: 16,
-        paddingVertical: 8,
-    },
-    text: {
-        paddingLeft: 8,
-    },
-});
+  row: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    paddingHorizontal: 16,
+    paddingVertical: 8,
+  },
+  text: {
+    paddingLeft: 8,
+  },
+})
--- app/components/DrawerWithLogoutButton.tsx
+++ app/components/DrawerWithLogoutButton.tsx
@@ -1,103 +1,115 @@
 import React from 'react'
 import { View, StyleSheet, ScrollView } from 'react-native'
-import { SafeAreaView, DrawerItemsProps } from 'react-navigation';
-import { Button, Avatar, IconButton } from "react-native-paper";
-import firebase from 'react-native-firebase';
-import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+import { SafeAreaView, DrawerItemsProps } from 'react-navigation'
+import { Button, Avatar, IconButton } from 'react-native-paper'
+import firebase from 'react-native-firebase'
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons'
 
-interface Props extends DrawerItemsProps { }
 interface RouteParams {
-    linkAccounts?: boolean,
-    title?: string,
-    onSignInSuccess?: () => void
+  linkAccounts?: boolean,
+  title?: string,
+  onSignInSuccess?: () => void
 }
 
-const DrawerWithLogoutButton = (props: Props) => {
-    const _handleOnLogoutPress = () => {
-        const routeName = 'SignInOptions'
-        const routeParams: RouteParams = {
-            title: 'Credentials',
-            linkAccounts: false,
-            onSignInSuccess: () => props.navigation.navigate('App')
-        }
-        firebase.auth()
-            .signOut()
-            .then(() => props.navigation.navigate(routeName, routeParams))
-            .catch((error) => console.log(error))
+const DrawerWithLogoutButton = (props: DrawerItemsProps) => {
+  const _handleOnLogoutPress = () => {
+    const routeName = 'SignInOptions'
+    const routeParams: RouteParams = {
+      title: 'Credentials',
+      linkAccounts: false,
+      onSignInSuccess: () => props.navigation.navigate('App'),
     }
+    firebase.auth()
+      .signOut()
+      .then(() => props.navigation.navigate(routeName, routeParams))
+      .catch((error) => { console.log(error) })
+  }
 
-    const _handleOnLinkAccountsPress = () => {
-        const routeName = 'LinkAccount'
-        const routeParams: RouteParams = {
-            title: 'Link Accounts',
-            linkAccounts: true
-        }
-        props.navigation.navigate(routeName, routeParams)
+  const _handleOnLinkAccountsPress = () => {
+    const routeName = 'LinkAccount'
+    const routeParams: RouteParams = {
+      title: 'Link Accounts',
+      linkAccounts: true,
     }
+    props.navigation.navigate(routeName, routeParams)
+  }
 
-    const _handleOpenBridges = () => {
-        const routeName = 'Bridges'
-        props.navigation.navigate(routeName);
-        props.navigation.closeDrawer();
-    }
+  const _handleOpenBridges = () => {
+    const routeName = 'Bridges'
+    props.navigation.navigate(routeName)
+    props.navigation.closeDrawer()
+  }
 
-    return (
-        < ScrollView contentContainerStyle={{ flex: 1, flexDirection: 'column', justifyContent: 'space-between' }}>
-            <SafeAreaView forceInset={{ top: 'always', horizontal: 'never' }}>
+  return (
+    <ScrollView contentContainerStyle={{ flex: 1, flexDirection: 'column', justifyContent: 'space-between' }}>
+      <SafeAreaView forceInset={{ top: 'always', horizontal: 'never' }}>
 
+        <View style={styles.container}>
+          {
+            firebase.auth().currentUser &&
+            <View
+              style={{
+                flexDirection: 'row',
+                width: '100%',
+                alignItems: 'center',
+                justifyContent: 'space-between',
 
-                <View style={styles.container}>
-                    {
-                        firebase.auth().currentUser &&
-                        <View style={{
-                            flexDirection: 'row',
-                            width: '100%',
-                            alignItems: "center",
-                            justifyContent: "space-between"
-
-                        }}>
-                            <IconButton
-                                icon='exit-to-app'
-                                onPress={_handleOnLogoutPress}
-                                size={20}
-                                color={`#6200ee`}
-                            ></IconButton>
-                            < IconButton
-                                icon='link'
-                                onPress={_handleOnLinkAccountsPress}
-                                size={20}
-                                color={`#6200ee`}
-                            ></IconButton>
-
-                        </View>
-                    }
-                    <View style={{ flexDirection: 'row', height: 200, width: '100%', alignItems: 'center', justifyContent: 'center' }}>
-                        <Avatar.Image style={{ selfAlign: 'center' }} size={150} source={{ uri: 'https://www.w3schools.com/howto/img_avatar.png' }} />
-                    </View>
-                    < Button
-                        style={styles.button}
-                        icon={() => <MaterialCommunityIcons name="bridge" size={24} color="white" />}
-                        mode="contained"
-                        onPress={_handleOpenBridges}
-                        color={`#6200ee`}
-                    >BRIDGES</Button>
-                </View>
-            </SafeAreaView>
-        </ScrollView >
-    );
+              }}
+            >
+              <IconButton
+                icon="exit-to-app"
+                onPress={_handleOnLogoutPress}
+                size={20}
+                color={`#6200ee`}
+              />
+              < IconButton
+                icon="link"
+                onPress={_handleOnLinkAccountsPress}
+                size={20}
+                color={`#6200ee`}
+              />
+            </View>
+          }
+          <View style={styles.containerAvatar}>
+            <Avatar.Image
+              style={{ selfAlign: 'center' }}
+              size={150}
+              source={{ uri: 'https://www.w3schools.com/howto/img_avatar.png' }}
+            />
+          </View>
+          < Button
+            style={styles.button}
+            icon={() => <MaterialCommunityIcons name="bridge" size={24} color="white" />}
+            mode="contained"
+            onPress={_handleOpenBridges}
+            color={`#6200ee`}
+          >
+            BRIDGES
+          </Button>
+        </View>
+      </SafeAreaView>
+    </ScrollView >
+  )
 }
 
 export default DrawerWithLogoutButton
 
 const styles = StyleSheet.create({
-    container: {
-        flexDirection: 'column',
-        alignItems: 'flex-start',
-        width: '100%',
-    },
-    button: {
-        width: '100%',
-        marginVertical: 3,
-        borderRadius: 0
-    },
-});
+  container: {
+    flexDirection: 'column',
+    alignItems: 'flex-start',
+    width: '100%',
+  },
+  containerAvatar: {
+    flexDirection: 'row',
+    height: 200,
+    width: '100%',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  button: {
+    width: '100%',
+    marginVertical: 3,
+    borderRadius: 0,
+  },
+})
