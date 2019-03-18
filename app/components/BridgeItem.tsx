6eb3cbf4101efb41e7c29608ab6957f3678c785e
dori claudino <dori.claudino@gmail.com>
2019-03-18 13:45:16 -0400

    TSLint

--- app/components/BridgeItem.tsx
+++ app/components/BridgeItem.tsx
@@ -1,39 +1,77 @@
-import * as React from 'react';
-import { View, Image } from 'react-native'
+import * as React from 'react'
+import { View, Image, StyleSheet } from 'react-native'
 import Entypo from 'react-native-vector-icons/Entypo'
-import { Text, TouchableRipple, Divider, List, Colors } from 'react-native-paper';
-import { capitalizeSentence } from '../helpers';
-import BridgeStatus from './BridgeStatus';
-import { Bridge } from 'store/bridge';
+import { Text, TouchableRipple, Divider, List, Colors } from 'react-native-paper'
+import { capitalizeSentence } from '../helpers'
+import BridgeStatus from './BridgeStatus'
+import { Bridge } from 'store/bridge'
 
 interface Props {
-    onPress: () => any;
-    bridge: Bridge
+  onPress: () => any;
+  bridge: Bridge
 }
 
 export default class extends React.Component<Props> {
-    render() {
-        const { bridge, onPress } = this.props
-        return (
-            <View>
-                <TouchableRipple onPress={onPress} style={{ padding: 4 }}>
-                    <View style={{ flex: 1, flexDirection: 'row' }}>
-                        <Image style={{ width: 100, height: 60, overflow: 'hidden', borderRadius: 10 }} source={{ uri: bridge.src }} />
-                        <View style={{ flex: 1, flexDirection: 'column', flexGrow: 2, paddingHorizontal: 4 }}>
-                            <Text numberOfLines={1} style={{ flex: 1, fontSize: 16, color: 'black', flexWrap: "wrap" }}>{capitalizeSentence(bridge.name)}</Text>
-                            <View style={{ flex: 1, flexDirection: 'row', justifyContent: 'flex-start', alignContent: 'flex-start', alignItems: 'flex-start' }}>
-                                <Text numberOfLines={1} style={{ flex: 1, fontSize: 10, color: 'gray', flexWrap: "wrap" }}>
-                                    {bridge.distance && bridge.distance.toFixed(2)}
-                                    <Entypo name="direction" size={10} color="gray" />
-                                </Text>
-                            </View>
-                            <BridgeStatus bridge={bridge} />
-                        </View>
-                        <List.Icon icon="keyboard-arrow-right" color={Colors.black} />
-                    </View>
-                </TouchableRipple>
-                <Divider />
+  render() {
+    const { bridge, onPress } = this.props
+    return (
+      <View>
+        <TouchableRipple onPress={onPress} style={{ padding: 4 }}>
+          <View style={styles.container}>
+            <Image style={styles.image} source={{ uri: bridge.src }} />
+            <View style={styles.innerContainer}>
+              <Text numberOfLines={1} style={styles.title}>{capitalizeSentence(bridge.name)}</Text>
+              <View style={styles.distanceContainer}>
+                <Text numberOfLines={1} style={styles.text}>
+                  {bridge.distance && bridge.distance.toFixed(2)}
+                  <Entypo name="direction" size={10} color="gray" />
+                </Text>
+              </View>
+              <BridgeStatus bridge={bridge} />
             </View>
-        )
-    }
+            <List.Icon icon="keyboard-arrow-right" color={Colors.black} />
+          </View>
+        </TouchableRipple>
+        <Divider />
+      </View>
+    )
+  }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: 'row',
+  },
+  image: {
+    width: 100,
+    height: 60,
+    overflow: 'hidden',
+    borderRadius: 10,
+  },
+  title: {
+    flex: 1,
+    fontSize: 16,
+    color: 'black',
+    flexWrap: 'wrap',
+  },
+  text: {
+    flex: 1,
+    fontSize: 10,
+    color: 'gray',
+    flexWrap: 'wrap',
+  },
+  distanceContainer: {
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'flex-start',
+    alignContent: 'flex-start',
+    alignItems: 'flex-start',
+  },
+  innerContainer: {
+    flex: 1,
+    flexDirection: 'column',
+    flexGrow: 2,
+    paddingHorizontal: 4,
+  },
+})
---
+++ app/helpers/index.ts
@@ -0,0 +1,68 @@
+import { GeolocationReturnType } from 'store/user'
+import { BridgeGeoLocation, Bridge } from 'store/bridge'
+
+/**
+ * @param {*} obj
+ * @description parse a object list to an array, create id property with list-key
+ * @example turns
+ * {"XlH_NcT9Jl2K": ...obj }
+ * ["id": "XlH_NcT9Jl2K", ...obj]
+ */
+export const parseToArrayWithId = (obj: any) => {
+  if (obj) {
+    return Object.keys(obj).map((key) => {
+      return { ...obj[key], id: key }
+    })
+  }
+  return []
+}
+
+export const capitalizeSentence = (sentence: string) => {
+  return sentence.toLowerCase().split(' ').map((a) => { a.charAt(0).toUpperCase().concat(a.substr(1)) }).join(' ')
+}
+
+const distance = (loc1: BridgeGeoLocation, loc2: GeolocationReturnType, unit: string) => {
+  const lat1 = loc1.lat
+  const lon1 = loc1.lng
+  const lat2 = loc2.coords.latitude
+  const lon2 = loc2.coords.longitude
+  if ((lat1 === lat2) && (lon1 === lon2)) {
+    return 0
+  }
+  const radlat1 = Math.PI * lat1 / 180
+  const radlat2 = Math.PI * lat2 / 180
+  const theta = lon1 - lon2
+  const radtheta = Math.PI * theta / 180
+  let dist = Math.sin(radlat1) * Math.sin(radlat2) + Math.cos(radlat1) * Math.cos(radlat2) * Math.cos(radtheta)
+  if (dist > 1) {
+    dist = 1
+  }
+  dist = Math.acos(dist)
+  dist = dist * 180 / Math.PI
+  dist = dist * 60 * 1.1515
+  if (unit === 'K') { dist = dist * 1.609344 }
+  if (unit === 'N') { dist = dist * 0.8684 }
+  return dist
+}
+
+const isNearby = (bridge: Bridge, loc2: GeolocationReturnType, definedDistance: number, unit: string) => {
+  const distanceBetween = distance(bridge.geo, loc2, unit)
+  bridge.distance = distanceBetween
+  return distanceBetween <= definedDistance
+}
+
+export const filterNameAndLocation = (
+  bridge: Bridge,
+  nameContains: string,
+  fromLocation: GeolocationReturnType,
+  nearbyDistance: number,
+  unit: string
+) => {
+  if (nameContains && bridge.name.toUpperCase().indexOf(nameContains.toUpperCase()) === -1) {
+    return false
+  }
+  if (fromLocation && nearbyDistance && !isNearby(bridge, fromLocation, nearbyDistance, unit)) {
+    return false
+  }
+  return true
+}
