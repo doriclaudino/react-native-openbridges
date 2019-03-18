685ed66c6fdae276c8453e4d2d75026fcecfcdb1
dori claudino <dori.claudino@gmail.com>
2019-03-18 12:57:05 -0400

    run tslint

--- App.tsx
+++ App.tsx
@@ -1,21 +1,21 @@
 /**
  * Sample React Native App
  * https://github.com/facebook/react-native
- * 
+ *
  * Generated with the TypeScript template
  * https://github.com/emin93/react-native-template-typescript
- * 
+ *
  * @format
  */
 
-import React, { Component } from 'react';
-import { Provider as ReduxProvider} from 'react-redux'
-import { store } from './app/store/BridgeStore';
-import { Provider as PaperProvider } from 'react-native-paper';
+import React from 'react'
+import { Provider as ReduxProvider } from 'react-redux'
+import { store } from './app/store/BridgeStore'
+import { Provider as PaperProvider } from 'react-native-paper'
 import RootNavigation from './app/navigation/RootNavigation'
-import firebase from 'react-native-firebase';
+import firebase from 'react-native-firebase'
 
-class App extends Component {
+class App extends React.Component {
   componentWillUnmount() {
     firebase.database().ref('bridges').off('value')
 
@@ -30,20 +30,20 @@ class App extends Component {
   }
 
   render() {
-    return (<PaperProvider>
-      <RootNavigation />
-    </PaperProvider>)
-  }
-}
-
-export default class AppContainer extends React.Component {
-  render() {
     return (
-      <ReduxProvider store={store}>
-        <App {...this.props} />
-      </ReduxProvider>
-    );
+      <PaperProvider>
+        <RootNavigation />
+      </PaperProvider>
+    )
   }
 }
 
+const AppContainer = (props: any) => {
+  return (
+    <ReduxProvider store={store}>
+      <App {...props} />
+    </ReduxProvider>
+  )
+}
 
+export default AppContainer
