9031b1df9cb1ca1bddcf75e0fa5be0d3560b88a9
dori claudino <dori.claudino@gmail.com>
2019-03-18 13:50:20 -0400

    TSLint

--- app/components/BridgeStatus.tsx
+++ app/components/BridgeStatus.tsx
@@ -1,149 +1,147 @@
-import * as React from 'react';
+import * as React from 'react'
 import { Text } from 'react-native'
-import { Card } from 'react-native-paper';
-import Moment from 'react-moment';
-import { Bridge, IStatus, IEvent } from 'store/bridge';
+import { Card } from 'react-native-paper'
+import Moment from 'react-moment'
+import { Bridge, IStatus, IEvent } from 'store/bridge'
 
-
+//tslint:disable:max-classes-per-file
 class Status implements IStatus {
-    when: Date;
-    color: string = '';
-    status: string = '';
-    constructor(date: Date) {
-        this.when = new Date(date)
-    }
+  when: Date
+  color: string = ''
+  status: string = ''
+  constructor(date: Date) {
+    this.when = new Date(date)
+  }
 }
 
 class UnkownStatus {
-    when?: Date | undefined = undefined;
-    color: string = '#9775AA';
-    status: string = 'NO HISTORY FOUND ';
+  when?: Date | undefined = undefined
+  color: string = '#9775AA'
+  status: string = 'NO HISTORY FOUND '
 }
 
 class OpenedStatus extends Status {
-    constructor(date: Date) {
-        super(date)
-        this.color = '#31E28D'
-        this.status = 'OPENED '
-    }
+  constructor(date: Date) {
+    super(date)
+    this.color = '#31E28D'
+    this.status = 'OPENED '
+  }
 }
 
 class ClosedStatus extends Status {
-    constructor(date: Date) {
-        super(date)
-        this.color = '#D12028'
-        this.status = 'CLOSED '
-    }
+  constructor(date: Date) {
+    super(date)
+    this.color = '#D12028'
+    this.status = 'CLOSED '
+  }
 }
 
 class TransictionColor extends Status {
-    constructor(date: Date) {
-        super(date)
-        this.color = '#F4AD07'
-    }
+  constructor(date: Date) {
+    super(date)
+    this.color = '#F4AD07'
+  }
 }
 
 class OpeningStatus extends TransictionColor {
-    constructor(date: Date) {
-        super(date)
-        this.status = 'OPENING '
-    }
+  constructor(date: Date) {
+    super(date)
+    this.status = 'OPENING '
+  }
 }
 
 class ClosingStatus extends TransictionColor {
-    constructor(date: Date) {
-        super(date)
-        this.status = 'CLOSING '
-    }
+  constructor(date: Date) {
+    super(date)
+    this.status = 'CLOSING '
+  }
 }
 
 interface Props {
-    bridge: Bridge
+  bridge: Bridge
 }
 
 export default class extends React.Component<Props> {
-    canChangeStatus = (statusThreshold: number, nextEvent: IEvent) => {
-        const now = new Date();
-        return nextEvent && +new Date(nextEvent.when) <= now.setMinutes(now.getMinutes() + statusThreshold)
-    }
+  canChangeStatus = (statusThreshold: number, nextEvent: IEvent) => {
+    const now = new Date()
+    return nextEvent && +new Date(nextEvent.when) <= now.setMinutes(now.getMinutes() + statusThreshold)
+  }
 
-    filterNextEvent = (bridge: Bridge) => {
-        const statusThreshold = bridge.statusThreshold;
-        if (bridge.events) {
-            const sortEvents = bridge.events
-                .filter(event => event && event.when && new Date() <= new Date(event.when))
-                .sort((a, b) => {
-                    return +new Date(a.when) - +new Date(b.when)
-                })
+  filterNextEvent = (bridge: Bridge) => {
+    const statusThreshold = bridge.statusThreshold
+    if (bridge.events) {
+      const sortEvents = bridge.events
+        .filter(event => event && event.when && new Date() <= new Date(event.when))
+        .sort((a, b) => {
+          return +new Date(a.when) - +new Date(b.when)
+        })
 
-            const nextEvent = sortEvents.shift()
+      const nextEvent = sortEvents.shift()
 
-            if (nextEvent) {
-                if (this.canChangeStatus(statusThreshold, nextEvent)) {
-                    if (this.isClose(nextEvent))
-                        return new ClosingStatus(nextEvent.when)
-                    else
-                        return new OpeningStatus(nextEvent.when)
-                }
-            }
+      if (nextEvent) {
+        if (this.canChangeStatus(statusThreshold, nextEvent)) {
+          if (this.isClose(nextEvent)) {
+            return new ClosingStatus(nextEvent.when)
+          }
+          return new OpeningStatus(nextEvent.when)
         }
-        return new UnkownStatus()
+      }
     }
+    return new UnkownStatus()
+  }
 
-    filterLastEvent = (bridge: Bridge) => {
-        if (bridge.events) {
-            const sortEvents = bridge.events
-                .filter(event => event && new Date() > new Date(event.when))
-                .sort((a, b) => { return +new Date(b.when) - +new Date(a.when); })
-            const lastEvent = sortEvents.shift()
+  filterLastEvent = (bridge: Bridge) => {
+    if (bridge.events) {
+      const sortEvents = bridge.events
+        .filter(event => event && new Date() > new Date(event.when))
+        .sort((a, b) => +new Date(b.when) - +new Date(a.when))
+      const lastEvent = sortEvents.shift()
 
-            if (lastEvent) {
-                if (this.isClose(lastEvent))
-                    return new ClosedStatus(lastEvent.when)
-                else
-                    return new OpenedStatus(lastEvent.when)
-            }
+      if (lastEvent) {
+        if (this.isClose(lastEvent)) {
+          return new ClosedStatus(lastEvent.when)
         }
+        return new OpenedStatus(lastEvent.when)
+      }
+    }
+    return new UnkownStatus()
+  }
+
+  getBridgeStatus = (bridge: Bridge) => {
+    try {
+      if (!bridge.events) {
         return new UnkownStatus()
+      }
+      const nextEvent = this.filterNextEvent(bridge)
+      if (!(nextEvent instanceof UnkownStatus)) {
+        return nextEvent
+      }
+      return this.filterLastEvent(bridge)
+    } catch (error) {
+      console.log(error)
     }
+    //by default we show OPEN and green at current datetime if nothing (lasts and nexts) events founded
+    return new UnkownStatus()
+  }
 
+  isClose = (event: IEvent) => {
+    return event.status.toLowerCase().indexOf('close') > -1
+  }
 
-    getBridgeStatus = (bridge: Bridge) => {
-        try {
-            if (!bridge.events)
-                return new UnkownStatus()
-
-            const nextEvent = this.filterNextEvent(bridge)
-            if (!(nextEvent instanceof UnkownStatus))
-                return nextEvent
-
-            return this.filterLastEvent(bridge)
-
-        } catch (error) {
-            console.log(error)
-        }
-        //by default we show OPEN and green at current datetime if nothing (lasts and nexts) events founded
-        return new UnkownStatus()
-    }
-
-    isClose = (event: IEvent) => {
-        return event.status.toLowerCase().indexOf('close') > -1
-    }
-
-    render() {
-        const { bridge } = this.props
-        const showStatus = this.getBridgeStatus(bridge)
-        return (
-            <Card.Content style={{ backgroundColor: showStatus.color, paddingBottom: 0 }}>
-                <Text>
-                    {showStatus.status}
-                    {showStatus.when &&
-                        <Moment element={Text} fromNow>
-                            {+showStatus.when}
-                        </Moment>
-                    }
-                </Text>
-            </Card.Content>
-        )
-    }
+  render() {
+    const { bridge } = this.props
+    const showStatus = this.getBridgeStatus(bridge)
+    return (
+      <Card.Content style={{ backgroundColor: showStatus.color, paddingBottom: 0 }}>
+        <Text>
+          {showStatus.status}
+          {showStatus.when &&
+            <Moment element={Text} fromNow={true}>
+              {+showStatus.when}
+            </Moment>
+          }
+        </Text>
+      </Card.Content>
+    )
+  }
 }
