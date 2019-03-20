83849eae28b392154a52f4017d893c9855c159a7
dori claudino <dori.claudino@gmail.com>
2019-03-19 21:21:23 -0400

    TSLint

--- app/reducers/index.js
+++
@@ -1,7 +0,0 @@
-import { combineReducers } from 'redux';
-import { bridgeReducer } from './bridgeReducer';
-import { userReducer } from './userReducer';
-export default combineReducers({
-    bridges: bridgeReducer,
-    ui: userReducer
-});
--- app/store/BridgeStore.js
+++
@@ -1,8 +0,0 @@
-import { createStore, applyMiddleware } from 'redux';
-import thunkMiddleware from 'redux-thunk';
-import reducers from '../reducers';
-
-const initialState = {}
-
-const store = createStore(reducers, initialState, applyMiddleware(thunkMiddleware));
-export { store };
